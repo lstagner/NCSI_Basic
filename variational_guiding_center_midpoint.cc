9f23a05e1aa57f0e09e87b40af6871743e7f772a
lellison <cellison@princeton.edu>
2014-02-05 08:56:21 -0500

    Drafted revised variational integrator

---
+++ variational_guiding_center_midpoint.cc
@@ -0,0 +1,253 @@
+/*!
+*******************************************************************************
+* \file variational_guiding_center_midpoint.cc
+*
+* \brief Implementation of integrator resulting from a midpoint discretization of the guiding center Lagrangian. Implements a step which interfaces with the base class integrators and defines the update rule. 
+*
+* \date Jan 2014
+* \author C. Leland Ellison
+*******************************************************************************
+*/
+
+#include "variational_guiding_center_midpoint.h"
+
+/*!
+ * Constructor - Initializes members and base class Integrator. Sets size of
+ x_history_ to have a number of rows equal to the dimension of the ODE system and number of columns equal to the number of steps in the multistep method. In this case, two.
+ *
+ * @param[in] kdt Numerical Step Size
+ * @param[in] kGuidingCenter ODE being modeled
+ * @param[in] kNewtonTolerance Error threshold for nonlinear solve
+ * @param[in] kMaxIterations Maximum number of nonlinear solve iterations
+ */
+VariationalGuidingCenterMidpoint::VariationalGuidingCenterMidpoint(
+					  const double kdt, 
+					  const GuidingCenter &kGuidingCenter, 
+					  const double kNewtonTolerance,
+					  const double kMaxIterations) 
+  : Integrator(kdt, model), kMu_(kGuidingCenter.kMu()), 
+    kNewtonTolerance_(kNewtonTolerance), kMaxIterations_(kMaxIterations),
+    needs_initialization_(true), x_history_(kGuidingCenter.kDimen(), 2){
+  x_history_.setZero(); // Just to be safe.
+}
+
+
+/*!
+ * \brief Advance t and x forward in time.
+ *
+ * If this is the first step, some additional initial conditions need to be generated. At present, this is performed using RK4. Otherwise, solve the implicit map defined by the discrete Euler-Lagrange equations.
+ * 
+ * @param[in] t Physical time at this step. For time-dependent systems.
+ * @param[in,out] x Vector of current coordinates. Will be updated to x(t+h). 
+ * @return Integer code which is 0 if successful. 
+ */
+int VariationalGuidingCenterMidpoint::Step(double &t, 
+						     Eigen::VectorXd &x){
+  if(needs_initialization_){
+
+    StoreHistory(t, x); // x_history_ --> [0, x0]
+
+    needs_initialization_ = false; // No longer need init
+
+    return InitialStep(t, x); // RK4 advance
+  }
+  else{
+    StoreHistory(t, x); // x_history_ --> [x_k-1, x_k]
+   
+    // Declare local variables used in nonlinear solve
+    Eigen::VectorXd error(kDimen_);
+    int n_iterations = 0;
+    Eigen::MatrixXd jacobian(kDimen_, kDimen_);
+
+    NewtonGuess(t, x); // Get estimate of new position. Advances (t,x).
+    UpdateRule(t, x, error); // Check how far t,x are from solution
+    
+    // Newton-Rhapson nonlinear solve
+    while((error.norm() > kNewtonTolerance)&&(n_iterations < kMaxIterations_)){
+      n_iterations++; // Increment how many times we've done this
+      Jacobian(t, x, jacobian);  // Fetch new Jacobian
+      x -= jacobian.inverse()*error; // Adjust x
+      UpdateRule(t, x, error); // Check new error
+    }
+    if (n_iterations == kMaxIterations_){
+      std::cout << "Newton solve did not converge!!" << std::endl;
+    }
+  } // end else 
+
+  return 0;
+}
+
+
+/*! StoreHistory updates member x_history_  by knocking off the
+ * oldest data and putting on the newest. 
+ * 
+ * @param[in] x New position
+ * @returns 0 upon success
+ */
+int VariationalGuidingCenterMidpoint::StoreHistory(const Eigen::VectorXd &kx){
+  // Shift everything back one
+  for(int i=0; i<n_steps_-1; ++i){
+    x_history_.col(i) = x_history_.col(i+1);
+  }
+  // Next, write the new data at the end
+  x_history_.col(n_steps_-1) = x;
+
+  return 0;
+}
+
+
+/*! InitialStep advances t and x using RK4. To be used while needs_initialization_ is true.
+ * 
+ * @param[in,out] t Simulation time. Advanced by kdt_. 
+ * @param[in,out] x Position. At in: x(t=t_k) At out: x(t=t_k+1)
+ * @returns 0 upon success
+ */
+int VariationalGuidingCenterMidpoint::InitialStep(double &t, 
+						  Eigen::VectorXd &x) const{
+  RungeKutta4 rk4(kdt_, model_);
+  return rk4.Step(t,x);
+}
+
+
+/*!
+ * \brief Function which should evaluate to zero(vector) when the algorithm is satisfied.
+ *
+ * @param[in] t Simulation time at proposed new position
+ * @param[in] x New position to test
+ * @param[out] error Error vector which is zero for satisfied update rule.
+ * @return Zero for success
+ */
+int VariationalGuidingCenterMidpoint::UpdateRule(const double kt,
+						 const Eigen::VectorXd &kx,
+						 Eigen::VectorXd &error) const
+{
+  //// Define intermediate variables
+  // Conventions: kp1 --> ``k plus 1"
+  //              km1 --> ``k minus 1"
+  //              kphalf --> ``k plus 1/2"
+  //              kmhalf --> ``k minus 1/2"
+  static double t_kphalf;
+  static double t_kmhalf;
+  static Eigen::Vector3d x_kphalf;
+  static Eigen::Vector3d x_kmhalf;
+  static Eigen::Vector3d delta_x_kphalf;
+  static Eigen::Vector3d delta_x_kmhalf;
+  static Eigen::Vector3d a_kphalf;
+  static Eigen::Vector3d a_kmhalf;
+  static Eigen::Vector3d b_hat_kphalf;
+  static Eigen::Vector3d b_hat_kmhalf;
+  static Eigen::MatrixXd grad_a_kphalf(3,3);
+  static Eigen::MatrixXd grad_a_kmhalf(3,3);
+  static Eigen::MatrixXd grad_b_hat_kphalf(3,3);
+  static Eigen::MatrixXd grad_b_hat_kmhalf(3,3);
+  static Eigen::Vector3d grad_mod_b_kphalf;
+  static Eigen::Vector3d grad_mod_b_kmhalf;
+  static Eigen::Vector3d grad_phi_kphalf;
+  static Eigen::Vector3d grad_phi_kmhalf;
+  static Eigen::MatrixXd grad_a_dag_kphalf(3,3);
+  static Eigen::MatrixXd grad_a_dag_kmhalf(3,3);
+  static Eigen::Vector3d a_dag_kphalf;
+  static Eigen::Vector3d a_dag_kmhalf;
+  
+  //// Update intermediate variables
+  // t coming in is t_kp1. Define t_kphalf, tkmhalf
+  t_kphalf = kt - 0.5*kdt_;
+  t_kmhalf = kt - 1.5*kdt_;
+
+  // Calculate x from history and kx (which is x_kp1)
+  // Recall x_history_ contains x_km1 in column 0 and x_k in column 1
+  // block<a,b>(c,d) selects a rows and b cols of a matrix starting from (c,d)
+  // head<a> picks first a elements of a vector
+  x_kphalf = 0.5*(x_history_.block<3,1>(0,1) + kx.head<3>());
+  x_kmhalf = 0.5*(x_history_.block<3,1>(0,0) + x_history_.block<3,1>(0,1));
+  delta_x_kphalf = kx.head<3>() - x_history_.block<3,1>(0,1);
+  delta_x_kmhalf = x_history_.block<3,1>(0,1) - x_history_.block<3,1>(0,0);
+
+  // Fields
+  em_fields_->VectorPotentialA(t_kphalf, x_kphalf, a_kphalf);
+  em_fields_->VectorPotentialA(t_kmhalf, x_kmhalf, a_kmhalf);
+  em_fields_->BHat(t_kphalf, x_kphalf, b_hat_kphalf);
+  em_fields_->BHat(t_kmhalf, x_kmhalf, b_hat_kmhalf);
+  em_fields_->GradA(t_kphalf, x_kphalf, grad_a_kphalf);
+  em_fields_->GradA(t_kmhalf, x_kmhalf, grad_a_kmhalf);
+  em_fields_->GradBHat(t_kphalf, x_kphalf, grad_b_hat_kphalf);
+  em_fields_->GradBHat(t_kmhalf, x_kmhalf, grad_b_hat_kmhalf);
+  em_fields_->GradModB(t_kphalf, x_kphalf, grad_mod_b_kphalf);
+  em_fields_->GradModB(t_kmhalf, x_kmhalf, grad_mod_b_kmhalf);
+  em_fields_->GradPhi(t_kphalf, x_kphalf, grad_phi_kphalf);
+  em_fields_->GradPhi(t_kmhalf, x_kmhalf, grad_phi_kmhalf);
+
+  // Convenience variables: A^\dagger = [A + u b]
+  a_dag_kphalf = a_kphalf + kx[3]*b_hat_kphalf;
+  a_dag_kmhalf = a_kmhalf + x_history_(3,1)*b_hat_kmhalf;
+  grad_a_dag_kphalf = grad_a_kphalf + kx[3]*grad_b_hat_kphalf;
+  grad_a_dag_kmhalf = grad_a_kmhalf + x_history_(3,1)*grad_b_hat_kmhalf;
+
+  //// Evaluate variational integrator update rule
+  // j = 0, 1, 2
+  // 1/2[A_{i,j}(k+1/2) + u_{k+1/2} b_{i,j}(k+1/2)]*[x_{k+1}^i - x_k^i] +
+  // 1/2[A_{i,j}(k-1/2) + u_{k-1/2} b_{i,j}(k-1/2)]*[x_{k}^i - x_{k-1}^i] - 
+  // [A_j(k+1/2) + u_{k+1/2} b_j(k+1/2) - A_j(k-1/2) - u_{k-1/2} b_j(k-1/2)] -
+  // h/2[muB_{,j}(k+1/2) + muB_{,j}(k-1/2) + phi_{,j}(k+1/2) + phi_{,j}(k-1/2)]
+  // NOTE: Transpose is to sum over proper index
+  error.head(3) = 0.5*grad_a_dag_kphalf.transpose()*delta_x_kphalf + 
+    0.5*grad_a_dag_kmhalf.transpose()*delta_x_kmhalf - 
+    (a_dag_kphalf - a_dag_kmhalf) - kdt_*0.5*(kMu_*(grad_mod_b_kphalf +
+    grad_mod_b_kmhalf) + grad_phi_kphalf + grad_phi_kmhalf);
+  
+  // j = 3
+  // b_i(k+1/2)*(x_{k+1}^i - x_k^i) - h*u_{k+1/2} = 0
+  error[3] = b_hat_kphalf.dot(delta_x_kphalf) - kdt_*kx[3];
+
+  return 0;
+}
+
+
+/*!
+ * \brief Guess for initializing NewtonSolver. Uses ForwardEuler.
+ *
+ * @param[in,out] t Simulation time. Advanced by step size kdt
+ * @param[in,out] x Position to advance to initial guess for solver
+ * @return Zero for success
+ */
+int VariationalGuidingCenterMidpoint::NewtonGuess(double &t, 
+						  Eigen::VectorXd &x) const {
+  // Stores output of VectorField
+  Eigen::VectorXd fx(kDimen_);
+
+  // Fetch f(x) in \dot{x} = f(x)
+  kGuidingCenter.VectorField(t,x,fx); 
+  t += kdt_;
+  x = x + kdt_*fx; 
+  return 0;
+}
+
+
+/*!
+ * \brief Calculates Jacobian matrix used in nonlinear solve. This base-class version uses a centered finite difference on the update rule.
+ * 
+ * @param[in] kt Simulation time at newest point.
+ * @param[in] kx Position at newest point.
+ * @param[out] jacobian derivative of the update rule w.r.t the new position
+ * @return Integer code which is 0 if successful. 
+ */
+int VariationalGuidingCenterMidpoint::Jacobian(const double kt, 
+					       const Eigen::VectorXd &kx, 
+					       Eigen::MatrixXd &jacobian) 
+  const{
+
+  double delta = 1e-5; // Step size for finite difference
+
+  static Eigen::VectorXd left_evaluation(kDimen_);
+  static Eigen::VectorXd right_evaluation(kDimen_);
+  static Eigen::MatrixXd eye = Eigen::MatrixXd::Identity(kDimen_, kDimen_);
+
+  // jacobian needs to be kDimen by kDimen!
+  for (int i=0; i<kDimen_; ++i){
+    UpdateRule(kt, kx + 0.5*delta*eye.col(i), right_evaluation);
+    UpdateRule(kt, kx - 0.5*delta*eye.col(i), left_evaluation);
+    jacobian.col(i) = (right_evaluation - left_evaluation)/delta;
+  }
+
+  return 0;
+}
---
+++ variational_guiding_center_midpoint.h
@@ -0,0 +1,51 @@
+/*!
+*******************************************************************************
+* \file variational_guiding_center_midpoint.h
+*
+* \brief Header for integrator resulting from a midpoint discretization of the guiding center Lagrangian. 
+*
+* \date Feb 2014
+* \author C. Leland Ellison
+*******************************************************************************
+*/
+
+#ifndef VARIATIONAL_GUIDING_CENTER_MIDPOINT_H_
+#define VARIATIONAL_GUIDING_CENTER_MIDPOINT_H_
+
+#include "guiding_center.h"
+#include "integrator.h"
+#include <stdlib.h>
+#include <Eigen/Dense>
+#include "runge-kutta4.h" // Used in InitialStep
+
+class VariationalGuidingCenterMidpoint : public Integrator{
+public:
+  VariationalGuidingCenterMidpoint(const double kdt, 
+				   const GuidingCenter &kGuidingCenter,
+				   const double kNewtonTolerance = 1e-12,
+				   const double kMaxIterations = 15);
+  // Method to advance (t_k, x_k) -> (t_k+1, x_k+1)
+  int Step(double &t, Eigen::VectorXd &x);
+  // Used when running multiple initial conditions to reset state
+  void Reset(){ needs_initialization_ = true; }
+protected:
+  // Pushes new point onto end of history. Drops oldest point.
+  int StoreHistory(const Eigen::VectorXd &kx);
+  // Method to perform time advance while more initial conditions are needed
+  int InitialStep(double &t, Eigen::VectorXd &x) const;
+  // Function which should evaluate to zero when algorithm is satisfied
+  int UpdateRule(const double kt, const Eigen::VectorXd &kx, 
+		 Eigen::VectorXd &error) const;
+  // Method to guess a next point (t,x). Uses forward euler here.
+  int NewtonGuess(double &t, Eigen::VectorXd &x) const;
+  // Calculates jacobian matrix used in nonlinear solve by finite difference
+  int Jacobian(const double kt, const Eigen::VectorXd &kx, 
+	       Eigen::MatrixXd &jacobian) const;
+  const double kMu_;  //!< Magnetic moment of particle
+  const double kNewtonTolerance_;  //!< Error threshold for nonlinear solve
+  const double kMaxIterations_;  //!< Maximum allowable Newton iterations
+  bool needs_initialization_;  //!< Flag for needing initial conditions
+  Eigen::MaxtrixXd x_history_;  //!< Stores previous positions
+};
+
+#endif // VARIATIONAL_GUIDING_CENTER_MIDPOINT_H_
