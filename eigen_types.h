93fedfbf7f424795206dde70a103d2b1a60af151
lellison <cellison@princeton.edu>
2014-07-24 12:10:56 -0400

    Convenience typedefs for Eigen

---
+++ eigen_types.h
@@ -0,0 +1,9 @@
+#ifndef EIGEN_TYPES_H_
+#define EIGEN_TYPES_H_
+
+typedef Eigen::Vector4d Vector4;
+typedef Eigen::Vector3d Vector3;
+typedef Eigen::Matrix4d Matrix4;
+typedef Eigen::Matrix3d Matrix3;
+
+#endif // EIGEN_TYPES_H_
